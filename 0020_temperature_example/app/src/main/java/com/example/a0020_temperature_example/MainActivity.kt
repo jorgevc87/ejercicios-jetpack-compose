c257f645672938aa9bc4f9b1079baa5f5882d94a
JorgeVC87 <jorgevc_87@outlook.com>
2024-04-15 14:28:32 -0500

    viewmodel exercises

--- 0020_temperature_example/app/src/main/java/com/example/a0020_temperature_example/MainActivity.kt
+++ 0020_temperature_example/app/src/main/java/com/example/a0020_temperature_example/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.example.a0020_temperature_example
 
+import android.health.connect.datatypes.units.Temperature
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a0020_temperature_example.ui.theme._0020_temperature_exampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,14 +36,23 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
+                    val temp = remember {
+                        "0.05F"
+                    }
 
+                    TemperatureRadioButton(
+                        selected = false,
+                        resId = R.string.fahrenheit,
+                        onClick = {
+
+                        })
                 }
             }
         }
     }
 }
 
-@Preview
+//Creating TextField to show temperature data
 @Composable
 fun TempratureTextField(
     temperature: MutableState<String>,
@@ -39,11 +60,101 @@ fun TempratureTextField(
     callback: () -> Unit
 ) {
 
-    TextField(value = temperature.value,
-        onValueChange =
+    TextField(
+        value = temperature.value,
+        onValueChange = {
+            temperature.value = it
+        },
+        modifier = modifier,
+        keyboardActions = KeyboardActions(onAny = {
+            callback()
+        }),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number,
+            imeAction = ImeAction.Done
+        ),
+        singleLine = true
     )
 
 }
+
+
+//Create radiobuttons
+@Composable
+fun TemperatureRadioButton(
+    selected: Boolean,
+    resId: Int,
+    onClick: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    /*
+    RadioButton and Text are simply added to row and vertically centered.
+    TemperatureRadioButton() receives a lambda expression with the onClick parameter.
+    */
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        //RadioButton
+        RadioButton(selected = selected, onClick = {
+            onClick(resId)
+        })
+
+        //Text on right side
+        Text(
+            text = stringResource(id = resId),
+            modifier = Modifier
+                .padding(start = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun TemperatureScaleButtonGroup(
+    selected: MutableState<Int>,
+    modifier: Modifier = Modifier
+) {
+    val sel = selected.value
+    val onClick = { resId: Int -> selected.value = resId }
+
+    Row(modifier = modifier) {
+        TemperatureRadioButton(
+            selected = sel == R.string.celsius,
+            resId = R.string.celsius,
+            onClick = onClick
+        )
+
+        TemperatureRadioButton(
+            selected = sel == R.string.fahrenheit,
+            resId = R.string.fahrenheit,
+            onClick = onClick,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
